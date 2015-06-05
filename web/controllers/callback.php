e2fd60262c3728010753495723f25fa4b640f1ad
Dipanjan Bhattacharjee <dipanjan.bhattacharjee@myglobalit.com>
2015-06-05 15:17:01 +0530

    Worked on parsing and saving callback response from SendGrid and Twilio

--- web/controllers/callback.php
+++ web/controllers/callback.php
@@ -8,24 +8,37 @@ use Symfony\Component\HttpFoundation\Response;
 $callback = $app['controllers_factory'];
 
 $callback->post('/', function (Request $request) use ($app) {
-
-    $oid = $request->request->get('oid', false);
+    
+    $sync = 0;
+    $origin = $status = $token = '';
+    $dated = new \DateTime();
+    
+    $oid = $request->query->get('oid', false);
     $params = json_decode($request->getContent());
+
     //twilio not sending in JSON format
     if ($oid) {
         $temp = $request->getContent().'&oid='.$oid;
         parse_str($temp, $tempArr);
         $params[] = $tempArr;
+        $origin = 'twilio';
+        $status = $tempArr['SmsStatus'];
+        $token  = $tempArr['SmsSid'];
+    } else {
+        $origin = 'sendgrid';
+        $status = $params[0]->event;
+        $token  = $params[0]->token;
     }
     $response = json_encode($params);
-    $sampleResponse = $response.'--'.print_r($request->request->all(), true)
-            .'--'.print_r($request->headers->all(), true).'---'.print_r($_SERVER, true);
-    
-    $sql = 'INSERT INTO `events` (`event_data`) VALUES( ? )';
 
-    $app['db']->executeQuery($sql, array($sampleResponse));
-    return $sampleResponse;
-    //return $app->json($post, 201);
+    //insert into table
+    $sql = 'INSERT INTO `events` (`data`, `dated`, `status`, `sync`, `origin`, `token`)
+        VALUES( ?, ?, ?, ?, ?, ? )';
+    $input = array($response, $dated->format('Y-m-d H:i:s'), $status, $sync, 
+        $origin, $token);
+    $app['db']->executeQuery($sql, $input);
+    
+    return $response;
 });
 
 return $callback;

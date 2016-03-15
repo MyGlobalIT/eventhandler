8ef4ac9ff1b4afb94bd4b89dd4d34b01459fa952
Dipanjan Bhattacharjee <dipanjan.bhattacharjee@myglobalit.com>
2016-03-15 12:26:26 +0530

    updated config file for new DB instance

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 
 define('DB_DRIVER', 'pdo_mysql');
 define('DB_CHARSET', 'utf8');
-define('DB_HOST', 'eztstaging.cyngvi5nsynu.us-east-1.rds.amazonaws.com');
+define('DB_HOST', 'eztstaging2.cyngvi5nsynu.us-east-1.rds.amazonaws.com');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'dev2010ezt');
 define('DB_PORT', 3306);
